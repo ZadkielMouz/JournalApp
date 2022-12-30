16638f59c09cb95079f376bf8ca168291b56fa0d
Angel Mouzaber <zadkielmouz98@gmail.com>
2022-12-30 01:34:32 -0400

    Build project

--- src/main.jsx
+++ src/main.jsx
@@ -5,14 +5,14 @@ import { JournalApp } from './JournalApp'
 import { Provider } from 'react-redux'
 import { store } from './store'
 import './styles.css'
-import { BrowserRouter } from 'react-router-dom'
+import { BrowserRouter, HashRouter } from 'react-router-dom'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <Provider store={store}>
-      <BrowserRouter>
+      <HashRouter>
         <JournalApp />
-      </BrowserRouter>
+      </HashRouter>
     </Provider>
   </React.StrictMode>,
 )
